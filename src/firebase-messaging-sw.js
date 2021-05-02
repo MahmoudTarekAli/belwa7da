ceea6caa6c5c761780a602e6bb7d8b7b58fc5933
Mahmoud Monem <mahmoudmonem1998@gmail.com>
2021-05-02 18:20:31 +0200

    fixed some errors

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,5 +1,3 @@
-const { SERVFAIL } = require('node:dns');
-
 importScripts('https://www.gstatic.com/firebasejs/8.4.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.4.2/firebase-messaging.js');
 firebase.initializeApp({
